0f2fb76022e216134cbce9f0bca3ea7f54f2c69c
Benoît Gastinne <Udyrfrykte@users.noreply.github.com>
2017-10-01 23:12:13 +0200

    Create libvmdk.rb

---
+++ Formula/libvmdk.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Libvmdk < Formula
+  desc "Library and tools to access the VMware Virtual Disk (VMDK) format"
+  homepage "https://github.com/libyal/libvmdk"
+  url "https://github.com/libyal/libvmdk/releases/download/20170226/libvmdk-alpha-20170226.tar.gz"
+  sha256 "85b451de90704c2ec4a071e1d7082aa177d17e6bc47484b59e39b448172bcc3f"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test libvmdk`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
