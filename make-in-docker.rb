3be4c98cfc64e20f0b22172d1a3d2cb98a2e87b1
Udyrfrykte <Udyrfrykte@users.noreply.github.com>
2017-10-24 22:40:53 +0200

    add formula for the make-in-docker script

---
+++ make-in-docker.rb
@@ -0,0 +1,13 @@
+class MakeInDocker < Formula
+  desc "Make in Docker tool (with completion)"
+  homepage "https://github.com/Udyrfrykte/make-in-docker/"
+  url "https://github.com/Udyrfrykte/make-in-docker.git",
+      :branch => "master"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "completion/bash/mid"
+    bin.install "mid"
+  end
+end
